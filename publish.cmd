01800a9687f25677d87ce9fd454cadd0b9280300
David Wood <david.wood@saabgroup.com>
2018-09-12 15:10:11 +0100

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=nonemergencyluisbot3-Web-Deploy.pubxml -p:Password=XiikmB5yeydcoaW965CzMHlpPkbKFSqXnglNNJbjjpnu5Wn6W5wxsRCwewfg
+
